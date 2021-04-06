b5105eaf58e3f52670718862e4e7218cfe4d32dd
Madhusudhan Srinivasa <me@madhums.me>
2021-04-06 13:10:13 +0200

    add eslint, prettier and pre-commit hooks

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: 'babel-eslint',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxBracketSameLine: true,
+      },
+    ],
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    quotes: ['error', 'single'],
+    camelcase: 'off',
+    'no-console': 'off',
+  },
+};
