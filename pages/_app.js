7ca58c16238a6cd8cc7a75c9fd62e0367c5d0d47
Madhusudhan Srinivasa <me@madhums.me>
2021-04-23 13:20:50 +0200

    add page loading indicator - nprgoress

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import PropTypes from 'proptypes';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import 'nprogress/nprogress.css';
+
+// Add loading indicator to the page
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 function App({ Component, pageProps }) {
   return (
