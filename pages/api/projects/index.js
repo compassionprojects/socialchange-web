21b68a6a3f83204e03f1ef34ad59a68ffd815eca
Madhusudhan Srinivasa <me@madhums.me>
2021-04-26 11:25:44 +0200

    add elastic search integration #3

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { knex } from '../../../db/config';
+import elastic from '../../../elastic';
 const knexPostgis = require('knex-postgis');
 const st = knexPostgis(knex);
 
@@ -8,8 +9,21 @@ export const defaultFilters = {
   'project_status.name': 'published', // only fetch 'published' projects
 };
 
+const searchFields = [
+  'title',
+  'description',
+  'intentions',
+  'outcomes',
+  'societal_change',
+  'topic',
+  'post',
+  'followup_title',
+  'followup_description',
+];
+
 export default async (req, res) => {
-  const { page, per_page, category_id } = req.query;
+  const { page, per_page, category_id, q } = req.query;
+  let results = { body: { responses: [] } };
   const limit = parseInt(per_page, 10) || 15;
   const offset = (parseInt(page, 10) - 1 || 0) * limit;
   const filter = {
@@ -17,6 +31,16 @@ export default async (req, res) => {
   };
 
   if (category_id) filter.category = category_id;
+  if (q) {
+    results = await elastic.msearch({
+      index: 'projects_index',
+      body: searchFields.reduce((init, field) => {
+        init.push({ index: 'projects_index' });
+        init.push({ query: { match: { [field]: q } } });
+        return init;
+      }, []),
+    });
+  }
 
   const projects = await knex
     .select([
@@ -50,5 +74,9 @@ export default async (req, res) => {
     .count('projects.id')
     .where(filter);
 
-  res.status(200).json({ projects, ...count });
+  res.status(200).json({
+    projects,
+    results: results.body.responses.flatMap((r) => r.hits.hits),
+    ...count,
+  });
 };
--- seeds/init.js
+++ seeds/init.js
@@ -1,5 +1,15 @@
-exports.seed = function (knex) {
+exports.seed = async function (knex) {
   const st = require('knex-postgis')(knex);
+  const elastic = require('../elastic');
+  try {
+    await elastic.delete({
+      id: 1,
+      index: 'projects_index',
+    });
+  } catch (e) {
+    console.log('Cant delete indexes');
+    console.log(e);
+  }
 
   return knex('users')
     .del()
@@ -63,91 +73,165 @@ exports.seed = function (knex) {
       knex('projects')
         .del()
         .then(function () {
-          return knex('projects').insert([
-            {
-              id: 1,
-              title: 'NVC school in London',
-              description:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-              intentions:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-              outcomes:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-              start_date: new Date(2020, 3, 2),
-              end_date: null,
-              geo: st.geomFromText('POINT(-0.09 51.505)'),
-              num_people: 20,
-              has_discussions: true,
-              created_by: 1,
-              updated_by: 1,
-              category: 1,
-              status: 2,
-            },
-          ]);
+          const title = 'NVC school in London';
+          const description =
+            'Chapman Flack, in reviewing a training video by Rosenberg, finds the presentation of key ideas "spell-binding" and the anecdotes "humbling and inspiring", notes the "beauty of his work", and his "adroitly doing fine attentive thinking" when interacting with his audience. Yet Flack wonders what to make of aspects of Rosenberg\'s presentation, such as his apparent "dim view of the place for thinking" and his building on Walter Wink\'s account of the origins of our way of thinking. To Flack, some elements of what Rosenberg says seem like pat answers at odds with the challenging and complex picture of human nature, history, literature, and art offer.[34]';
+          const intentions =
+            'Flack notes a distinction between the "strong sense" of Nonviolent Communication as a virtue that is possible with care and attention, and the "weak sense," a mimicry of this born of ego and haste. The strong sense offers a language to examine one\'s thinking and actions, support understanding, bring one\'s best to the community, and honor one\'s emotions. In the weak sense, one may take the language as rules and use these to score debating points, label others for political gain, or insist that others express themselves in this way. Though concerned that some of what Rosenberg says could lead to the weak sense, Flack sees evidence confirming that Rosenberg understands the strong sense in practice. Rosenberg\'s work with workshop attendees demonstrates "the real thing." Yet Flack warns that "the temptation of the weak sense will not be absent." As an antidote, Flack advises, "Be conservative in what you do, be liberal in what you accept from others," (also known as the robustness principle) and guard against the "metamorphosis of nonviolent communication into subtle violence done in its name."[34]';
+          const outcomes =
+            "Marion Little examines theoretical frameworks related to NVC. The influential interest-based model for conflict resolution, negotiation, and mediation developed by Fisher, Ury, and Patton at the Harvard Negotiation Project and at the Program on Negotiation in the 1980s appears to have some conceptual overlap with NVC, although neither model references the other.[9]:31–35 Little suggests The Gordon Model for Effective Relationships (1970) as a likely precursor to both NVC and interest-based negotiation, based on conceptual similarities, if not any direct evidence of a connection.[9]:35–41 Like Rosenberg, Gordon had worked with Carl Rogers, so the models' similarities may reflect common influences.[9]:35";
+          return knex('projects')
+            .insert([
+              {
+                id: 1,
+                title,
+                description,
+                intentions,
+                outcomes,
+                start_date: new Date(2020, 3, 2),
+                end_date: null,
+                geo: st.geomFromText('POINT(-0.09 51.505)'),
+                num_people: 20,
+                has_discussions: true,
+                created_by: 1,
+                updated_by: 1,
+                category: 1,
+                status: 2,
+              },
+            ])
+            .then(() =>
+              elastic
+                .index({
+                  index: 'projects_index',
+                  id: 1,
+                  body: {
+                    id: 1,
+                    title,
+                    description,
+                    intentions,
+                    outcomes,
+                  },
+                })
+                .catch((e) => console.log(e.meta.body.error))
+            );
         })
     )
     .then(() =>
       knex('topics')
         .del()
         .then(function () {
-          return knex('topics').insert([
-            {
-              id: 1,
-              title: 'How did you get consent?',
-              project: 1,
-              created_by: 1,
-              updated_by: 1,
-            },
-            {
-              id: 2,
-              title: 'How are people responding?',
-              project: 1,
-              created_by: 1,
-              updated_by: 1,
-            },
-          ]);
+          return knex('topics')
+            .insert([
+              {
+                id: 1,
+                title: 'How did you get consent?',
+                project: 1,
+                created_by: 1,
+                updated_by: 1,
+              },
+              {
+                id: 2,
+                title: 'How are people responding?',
+                project: 1,
+                created_by: 1,
+                updated_by: 1,
+              },
+            ])
+            .then(() =>
+              elastic
+                .index({
+                  index: 'projects_index',
+                  id: 1,
+                  body: {
+                    topics: [
+                      { topic: 'How did you get consent?' },
+                      { topic: 'How are people responding?' },
+                    ],
+                  },
+                })
+                .catch((e) => console.log(e.meta.body.error))
+            );
         })
     )
     .then(() =>
       knex('followups')
         .del()
         .then(() =>
-          knex('followups').insert([
-            {
-              id: 1,
-              title: 'Update after a month',
-              description:
-                'Lorem ipsum dolar sit amet when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap',
-              project: 1,
-              date: new Date(2020, 6, 1),
-              created_by: 1,
-              updated_by: 1,
-            },
-          ])
+          knex('followups')
+            .insert([
+              {
+                id: 1,
+                title: 'Update after a month',
+                description:
+                  'Lorem ipsum dolar sit amet when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap',
+                project: 1,
+                date: new Date(2020, 6, 1),
+                created_by: 1,
+                updated_by: 1,
+              },
+            ])
+            .then(() =>
+              elastic
+                .index({
+                  index: 'projects_index',
+                  id: 1,
+                  body: {
+                    followups: [
+                      {
+                        title: 'Update after a month',
+                        description:
+                          'Lorem ipsum dolar sit amet when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap',
+                      },
+                    ],
+                  },
+                })
+                .catch((e) => console.log(e.meta.body.error))
+            )
         )
     )
     .then(() =>
       knex('posts')
         .del()
         .then(function () {
-          return knex('posts').insert([
-            {
-              id: 1,
-              post:
-                'I can imagine it must have been really a difficult conversation. How did you go about getting consent?',
-              topic: 1,
-              created_by: 1,
-              updated_by: 1,
-            },
-            {
-              id: 2,
-              post:
-                'How did you even manage to mediate this? Wow! I am really amazed. Did you get the consent after phase 1?',
-              topic: 1,
-              created_by: 1,
-              updated_by: 1,
-            },
-          ]);
+          return knex('posts')
+            .insert([
+              {
+                id: 1,
+                post:
+                  'I can imagine it must have been really a difficult conversation. How did you go about getting consent?',
+                topic: 1,
+                created_by: 1,
+                updated_by: 1,
+              },
+              {
+                id: 2,
+                post:
+                  'How did you even manage to mediate this? Wow! I am really amazed. Did you get the consent after phase 1?',
+                topic: 1,
+                created_by: 1,
+                updated_by: 1,
+              },
+            ])
+            .then(() =>
+              elastic
+                .index({
+                  index: 'projects_index',
+                  id: 1,
+                  body: {
+                    posts: [
+                      {
+                        post:
+                          'I can imagine it must have been really a difficult conversation. How did you go about getting consent?',
+                      },
+                      {
+                        post:
+                          'How did you even manage to mediate this? Wow! I am really amazed. Did you get the consent after phase 1?',
+                      },
+                    ],
+                  },
+                })
+                .catch((e) => console.log(e.meta.body.error))
+            );
         })
     );
 };
