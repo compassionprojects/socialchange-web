14ec82cd3b588d317d2ff3f9090bd41b6b6b4db8
Madhusudhan Srinivasa <me@madhums.me>
2021-05-06 17:24:45 +0200

    fix import

--- pages/api/projects/create.js
+++ pages/api/projects/create.js
@@ -2,7 +2,7 @@ import db from '../../../db';
 import elastic from '../../../elastic';
 import jwt from 'next-auth/jwt';
 import knexPostgis from 'knex-postgis';
-import { mapCountry } from '../../../../lib';
+import { mapCountry } from '../../../lib';
 
 const st = knexPostgis(db);
 const secret = process.env.JWT_SECRET;
